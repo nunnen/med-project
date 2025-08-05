05f077bff471d81e1294ac846ba0e261aa5b5798
Niyaz <niazsajdullin@gmail.com>
2025-08-05 19:16:55 +0500

    EDIT repositories
    package to public

--- src/main/java/com/vunnen/med/repository/AppointmentRepository.java
+++ src/main/java/com/vunnen/med/repository/AppointmentRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
 }
--- src/main/java/com/vunnen/med/repository/PatientRepository.java
+++ src/main/java/com/vunnen/med/repository/PatientRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface PatientRepository extends JpaRepository<Patient, Integer> {
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
 }
